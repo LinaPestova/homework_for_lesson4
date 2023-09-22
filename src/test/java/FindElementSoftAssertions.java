15fd665071f0c9b9706defd708c9c9fac5ef56b2
Alina Pestova <ali.pestova@yandex.ru>
2023-09-22 22:31:12 +0300

    first and finish commit

---
+++ src/test/java/FindElementSoftAssertions.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.BeforeAll;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class FindElementSoftAssertions {
+    @BeforeAll
+        static void beforeAll() {
+        Configuration.baseUrl="https://github.com/selenide/selenide/wiki";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+    }
+@Test
+    void searchJunit(){
+        open (baseUrl);
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+            "class Tests {\n" +
+            "  @Test\n" +
+            "  void test() {\n" +
+            "    Configuration.assertionMode = SOFT;\n" +
+            "    open(\"page.html\");\n" +
+            "\n" +
+            "    $(\"#first\").should(visible).click();\n" +
+            "    $(\"#second\").should(visible).click();\n" +
+            "  }\n" +
+            "}"));
+
+    $(".markdown-body").shouldHave(text("class Tests {\n" +
+            "  @RegisterExtension \n" +
+            "  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();\n" +
+            "\n" +
+            "  @Test\n" +
+            "  void test() {\n" +
+            "    Configuration.assertionMode = SOFT;\n" +
+            "    open(\"page.html\");\n" +
+            "\n" +
+            "    $(\"#first\").should(visible).click();\n" +
+            "    $(\"#second\").should(visible).click();\n" +
+            "  }\n" +
+            "}"));
+}
+}
